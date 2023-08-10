01fe309deeb64a5470b89131c950606b04b045cd
Martin Chapman <contact@martinchapman.co.uk>
2023-08-10 05:39:44 +0100

    Update made by Phenoflow

---
+++ python/septicaemia-sepsis---secondary.py
@@ -0,0 +1,25 @@
+# Kuan V, Denaxas S, Gonzalez-Izquierdo A, Direk K, Bhatti O, Husain S, Sutaria S, Hingorani M, Nitsch D, Parisinos C, Lumbers T, Mathur R, Sofat R, Casas JP, Wong I, Hemingway H, Hingorani A, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"A02.1","system":"icd10"},{"code":"A22.7","system":"icd10"},{"code":"A26.7","system":"icd10"},{"code":"A32.7","system":"icd10"},{"code":"A40","system":"icd10"},{"code":"A41","system":"icd10"},{"code":"A42.7","system":"icd10"},{"code":"B37.7","system":"icd10"},{"code":"P36","system":"icd10"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('septicaemia-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["septicaemia-sepsis---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["septicaemia-sepsis---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["septicaemia-sepsis---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
